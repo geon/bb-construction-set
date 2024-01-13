08bff66d3b50d2e55b9842b06e47578feddbde7a
Victor Widell <victor.widell@gmail.com>
2024-01-13 19:48:20 +0100

    Refactor. Draw each level to a level-sized ImageData.

--- src/draw-levels-to-canvas.tsx
+++ src/draw-levels-to-canvas.tsx
@@ -9,7 +9,10 @@ export function drawLevelsToCanvas(
 		return;
 	}
 
-	const image = new ImageData(levelWidth * 10, levelHeight * 10);
+	canvas.width = levelWidth * 10;
+	canvas.height = levelHeight * 10;
+
+	const image = new ImageData(levelWidth, levelHeight);
 	for (let levelY = 0; levelY < 10; ++levelY) {
 		for (let levelX = 0; levelX < 10; ++levelX) {
 			const levelIndex = levelY * 10 + levelX;
@@ -26,21 +29,14 @@ export function drawLevelsToCanvas(
 				for (let tileX = 0; tileX < levelWidth; ++tileX) {
 					const tileIndex = tileY * levelWidth + tileX;
 					const tileIsSet = levels[levelIndex].tiles[tileIndex];
-					const pixelIndex =
-						levelY * 10 * levelWidth * levelHeight +
-						levelX * levelWidth +
-						tileY * (levelWidth * 10) +
-						tileX;
-					image.data[pixelIndex * 4 + 0] = tileIsSet ? randomColor[0] : 0;
-					image.data[pixelIndex * 4 + 1] = tileIsSet ? randomColor[1] : 0;
-					image.data[pixelIndex * 4 + 2] = tileIsSet ? randomColor[2] : 0;
-					image.data[pixelIndex * 4 + 3] = 255;
+					image.data[tileIndex * 4 + 0] = tileIsSet ? randomColor[0] : 0;
+					image.data[tileIndex * 4 + 1] = tileIsSet ? randomColor[1] : 0;
+					image.data[tileIndex * 4 + 2] = tileIsSet ? randomColor[2] : 0;
+					image.data[tileIndex * 4 + 3] = 255;
 				}
 			}
+
+			ctx.putImageData(image, levelX * levelWidth, levelY * levelHeight);
 		}
 	}
-
-	canvas.width = image.width;
-	canvas.height = image.height;
-	ctx.putImageData(image, 0, 0);
 }
