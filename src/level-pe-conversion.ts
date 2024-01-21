53198cb3d097b95f0f69519adea7a3f9ee6233b4
Victor Widell <victor.widell@gmail.com>
2024-01-21 20:34:14 +0100

    Added rudimentary PETSCII Editor export.

---
+++ src/level-pe-conversion.ts
@@ -0,0 +1,118 @@
+import { Level } from "./level";
+import { CharBitmap, PeFileData } from "./pe-file";
+import { Sprites } from "./sprite";
+import { CharsetChar } from "./charset-char";
+
+export function levelsToPeFileData(data: {
+	levels: readonly Level[];
+	sprites: Sprites;
+}): PeFileData {
+	const now = new Date().getTime();
+	const emptyChar: CharBitmap = [0, 0, 0, 0, 0, 0, 0, 0];
+
+	return {
+		app: "PETSCII Editor",
+		url: "http://petscii.krissz.hu/",
+		meta: {
+			name: "Bubble Bobble c64",
+			authorName: "",
+			editorVersion: "3.0",
+			fileFormatVersion: "3.0",
+			createTime: now,
+			lastSaveTime: now,
+		},
+		options: {
+			palette: "pepto-pal",
+			crtFilter: "scanlines",
+			fileName: "bubble-bobble-c64",
+			saveCounter: 1,
+			concatSaveCounter: "yes",
+			autosave: "yes",
+			autosaveInterval: 10,
+			keyboardLayout: "us",
+			firstKeydown: "draw",
+			firstClick: "draw",
+			tooltips: "yes",
+			cursorFollow: "yes",
+		},
+		clipboards: { screenEditor: [], charsetEditor: false, spriteEditor: false },
+		charsets: data.levels.map(
+			(level, levelIndex): PeFileData["charsets"][number] => ({
+				name: "Level " + (levelIndex + 1),
+				mode: "multicolor",
+				bgColor: 0,
+				charColor: 0, // Black to not tempt using it.
+				multiColor1: level.bgColorLight,
+				multiColor2: level.bgColorDark,
+				// `bitmaps.length` should be exactly 256.
+				bitmaps: padRight(
+					[level.platformChar, ...(level.sidebarChars ?? [])].map(
+						levelCharToPeChar
+					),
+					256,
+					emptyChar
+				),
+			})
+		),
+		screens: data.levels.map(
+			(level, levelIndex): PeFileData["screens"][number] => ({
+				name: "Level " + (levelIndex + 1),
+				mode: "multicolor",
+				sizeX: 40,
+				sizeY: 25,
+				colorBorder: 0,
+				colorBg: 0,
+				colorChar: 13, // Multicolor green for bubbles.
+				multiColor1: level.bgColorLight,
+				multiColor2: level.bgColorDark,
+				extBgColor1: 0,
+				extBgColor2: 0,
+				extBgColor3: 0,
+				spriteMultiColor1: 1, // White
+				spriteMultiColor2: 2, // Dark red
+				spritesInBorder: "hidden",
+				spritesVisible: true,
+				characterSet: levelIndex,
+				// `charData.length` should match `sizeY` and `charData[n].length` should match `sizeX`.
+				charData: chunk(level.tiles, 32)
+					.map((row) => padRight(row, 40, false))
+					.map((row) => row.map((tile) => (tile ? 0 : 20))),
+				// Same for colorData.
+				// Multicolor green for bubbles.
+				colorData: padRight([], 25, padRight([], 40, 13)),
+				sprites: [],
+				undoStack: [],
+				redoStack: [],
+			})
+		),
+		spriteSets: [],
+	};
+}
+
+function levelCharToPeChar(char: CharsetChar): CharBitmap {
+	return char.lines.map((line): CharBitmap[number] =>
+		line.reduce<CharBitmap[number]>(
+			(soFar, pixel, currentIndex) =>
+				soFar + (pixel << ((3 - currentIndex) * 2)),
+			0
+		)
+	) as CharBitmap;
+}
+
+function padRight<T>(array: readonly T[], length: number, padding: T): T[] {
+	const result = array.slice();
+	while (result.length < length) {
+		result.push(padding);
+	}
+	return result;
+}
+
+function chunk<T>(array: readonly T[], chunkLength: number): T[][] {
+	const chunks: T[][] = [];
+	let start = 0;
+	do {
+		chunks.push(array.slice(start, start + chunkLength));
+		start += chunkLength;
+	} while (start < array.length);
+	return chunks;
+}
---
+++ src/pe-file.ts
@@ -0,0 +1,109 @@
+export type CharBitmap = [
+	number,
+	number,
+	number,
+	number,
+	number,
+	number,
+	number,
+	number
+];
+
+type Bit = 0 | 1;
+type SpriteBitmapByte = [Bit, Bit, Bit, Bit, Bit, Bit, Bit, Bit];
+
+type ColorMode = "single" | "multicolor";
+
+export interface PeFileData {
+	app: "PETSCII Editor";
+	url: "http://petscii.krissz.hu/";
+	meta: {
+		name: string;
+		authorName: string;
+		editorVersion: "3.0";
+		fileFormatVersion: "3.0";
+		createTime: number;
+		lastSaveTime: number;
+	};
+	options: {
+		palette: "pepto-pal";
+		crtFilter: "scanlines";
+		fileName: string;
+		saveCounter: 1;
+		concatSaveCounter: "yes";
+		autosave: "yes";
+		autosaveInterval: 10;
+		keyboardLayout: "us";
+		firstKeydown: "draw";
+		firstClick: "draw";
+		tooltips: "yes";
+		cursorFollow: "yes";
+	};
+	clipboards: { screenEditor: []; charsetEditor: false; spriteEditor: false };
+	charsets: {
+		name: string;
+		mode: ColorMode;
+		bgColor: number;
+		charColor: number;
+		multiColor1: number;
+		multiColor2: number;
+		// `bitmaps.length` should be exactly 256.
+		bitmaps: CharBitmap[];
+	}[];
+	screens: {
+		name: string;
+		mode: ColorMode;
+		sizeX: 40;
+		sizeY: 25;
+		colorBorder: number;
+		colorBg: number;
+		colorChar: number;
+		multiColor1: number;
+		multiColor2: number;
+		extBgColor1: number;
+		extBgColor2: number;
+		extBgColor3: number;
+		spriteMultiColor1: number;
+		spriteMultiColor2: number;
+		spritesInBorder: "hidden";
+		spritesVisible: true;
+		characterSet: number;
+		// `charData.length` should match `sizeY` and `charData[n].length` should match `sizeX`.
+		charData: number[][];
+		// Same for colorData.
+		colorData: number[][];
+		sprites: {
+			setId: number;
+			uid: string;
+			x: number;
+			y: number;
+			color: number;
+			expandX: false;
+			expandY: false;
+			priority: "front";
+		}[];
+		undoStack: [];
+		redoStack: [];
+	}[];
+	spriteSets: {
+		name: string;
+		sprites: {
+			uid: string;
+			mode: ColorMode;
+			colorBg: 6;
+			colorSprite: 14;
+			multiColor1: 2;
+			multiColor2: 6;
+			expandX: false;
+			expandY: false;
+			// `bitmapData.length` should be 3*21 = 63.
+			bitmapData: SpriteBitmapByte[];
+		}[];
+		undoStack: [];
+		redoStack: [];
+	}[];
+}
+
+export function serializePeFileData(data: PeFileData): string {
+	return JSON.stringify({ app: "PETSCII Editor", data: JSON.stringify(data) });
+}
