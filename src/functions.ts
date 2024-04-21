6a87db13fab7e3e45ee01535cb320be8ff669bc0
Victor Widell <victor.widell@gmail.com>
2024-04-21 12:11:07 +0200

    Refactor. Moved functions to reusable file.

---
+++ src/functions.ts
@@ -0,0 +1,21 @@
+export function padRight<T>(
+	array: readonly T[],
+	length: number,
+	padding: T
+): T[] {
+	const result = array.slice();
+	while (result.length < length) {
+		result.push(padding);
+	}
+	return result;
+}
+
+export function chunk<T>(array: readonly T[], chunkLength: number): T[][] {
+	const chunks: T[][] = [];
+	let start = 0;
+	do {
+		chunks.push(array.slice(start, start + chunkLength));
+		start += chunkLength;
+	} while (start < array.length);
+	return chunks;
+}
