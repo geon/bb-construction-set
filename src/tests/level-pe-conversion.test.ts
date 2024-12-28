e48ddfb2513f1aabcfbd08720a90f0aa42f49cd5
Victor Widell <victor.widell@gmail.com>
2024-12-28 19:43:22 +0100

    Test: peFileDataToLevels & levelsToPeFileData

---
+++ src/tests/level-pe-conversion.test.ts
@@ -0,0 +1,34 @@
+import { expect, test } from "vitest";
+import { levelsToPeFileData, peFileDataToLevels } from "../level-pe-conversion";
+import { deserializePeFileData } from "../pe-file";
+import { readFileSync } from "fs";
+import { numSpriteBytes, spriteCounts, Sprites } from "../sprite";
+
+test("peFileDataToLevels & levelsToPeFileData", () => {
+	const peFileData = deserializePeFileData(
+		readFileSync(__dirname + "/level-01.pe", "utf8")
+	);
+
+	const levels = peFileDataToLevels(peFileData);
+
+	const generatedPeFileData = levelsToPeFileData({
+		levels,
+		// Dummy data, not tested.
+		sprites: Object.fromEntries(
+			Object.entries(spriteCounts).map(([characterName, count]) => [
+				characterName,
+				Array(count).fill({
+					bitmap: Array(numSpriteBytes).fill(0),
+				}),
+			])
+		) as Sprites,
+	});
+
+	// Dummy data, not tested.
+	peFileData.spriteSets = generatedPeFileData.spriteSets = [];
+
+	// Has datetime.
+	peFileData.meta = generatedPeFileData.meta;
+
+	expect(generatedPeFileData).toStrictEqual(peFileData);
+});
