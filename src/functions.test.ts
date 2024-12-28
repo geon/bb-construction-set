f6c087d71ececb02b76fbc3a077fb4609ea6b78b
Victor Widell <victor.widell@gmail.com>
2024-12-28 14:51:56 +0100

    Test: chunk

--- src/functions.test.ts
+++ src/functions.test.ts
@@ -1,6 +1,10 @@
 import { expect, test } from "vitest";
-import { padRight } from "./functions";
+import { chunk, padRight } from "./functions";
 
 test("padRight", () => {
 	expect(padRight([1, 2, 3], 5, 0)).toStrictEqual([1, 2, 3, 0, 0]);
+});
+
+test("chunk", () => {
+	expect(chunk([1, 2, 3, 4, 5], 2)).toStrictEqual([[1, 2], [3, 4], [5]]);
 });
