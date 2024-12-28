e8e6cbcd62c0569b71eda9c24e0e3940568c3693
Victor Widell <victor.widell@gmail.com>
2024-12-28 14:11:15 +0100

    Test: mixColors

--- src/color.test.ts
+++ src/color.test.ts
@@ -1,5 +1,5 @@
 import { expect, test } from "vitest";
-import { Color, hexToRgb } from "./color";
+import { Color, hexToRgb, mixColors } from "./color";
 
 test("hexToRgb", () => {
 	const color: Color = {
@@ -17,4 +17,30 @@ test("hexToRgb", () => {
 	const rgbFromInt = hexToRgb(hexInt);
 
 	expect(rgbFromInt).toStrictEqual(color);
+});
+
+test("mixColors", () => {
+	const colors: Color[] = [
+		{
+			r: 30,
+			g: 0,
+			b: 0,
+		},
+		{
+			r: 0,
+			g: 30,
+			b: 0,
+		},
+		{
+			r: 0,
+			g: 0,
+			b: 30,
+		},
+	];
+
+	expect(mixColors(colors)).toStrictEqual({
+		r: 10,
+		g: 10,
+		b: 10,
+	});
 });
