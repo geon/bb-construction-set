5eb53ae98674bdbfeea5c05ca91b75a9796f9138
Victor Widell <victor.widell@gmail.com>
2025-01-21 20:27:35 +0100

    Refactor. Moved data locations to new file.

--- src/parse-prg.ts
+++ src/parse-prg.ts
@@ -6,6 +6,18 @@ import {
 } from "./charset-char";
 import { chunk } from "./functions";
 import {
+	itemCharsArrays,
+	bitmapArrayAddress,
+	sidebarCharArrayAddress,
+	monsterArrayAddress,
+	windCurrentsArrayAddress,
+	bgColorMetadataArrayAddress,
+	holeMetadataArrayAddress,
+	symmetryMetadataArrayAddress,
+	platformCharArrayAddress,
+	spriteBitmapArrayAddress,
+} from "./prg/data-locations";
+import {
 	BubbleCurrentDirection,
 	BubbleCurrentRectangle,
 	BubbleCurrentRectangleOrSymmetry,
@@ -67,91 +79,6 @@ function setPrgByteAtAddress(
 function isBitSet(byte: number, bitIndex: number): boolean {
 	return !!(byte & (0b10000000 >> bitIndex));
 }
-
-const platformCharArrayAddress = 0xc26e;
-const sidebarCharArrayAddress = 0xbb0e;
-const bgColorMetadataArrayAddress = 0xff30;
-const holeMetadataArrayAddress = 0xc58e;
-const symmetryMetadataArrayAddress = 0xff94;
-const bitmapArrayAddress = 0xc5f2;
-const monsterArrayAddress = 0xae51;
-const windCurrentsArrayAddress = 0xb695;
-
-const spriteBitmapArrayAddress = 0x5800;
-const itemCharsArrays = [
-	// Blow bubble animation.
-	{
-		address: 0x8000,
-		numItems: 144 / 4,
-		withMask: true,
-	},
-	// Bubble pop.2 frames + position.
-	{
-		address: 0x8980,
-		numItems: 12,
-		withMask: true,
-	},
-	// Baron von Blubba.
-	{
-		address: 0x8f00,
-		numItems: 12,
-	},
-	// Special bubbles: Water, fire, lightning.
-	{
-		address: 0x8f00 + 4 * 8 * 12,
-		numItems: 18,
-	},
-	// Lightning
-	{
-		address: 0x8f00 + 4 * 8 * (12 + 18),
-		numItems: 2,
-		withMask: true,
-	},
-	// Fire
-	{
-		address: 0x8f00 + 4 * 8 * (12 + 18 + 2),
-		numItems: 12,
-		withMask: true,
-	},
-	// E-X-T-N-D bubbles.
-	{
-		address: 0x8f00 + 4 * 8 * (12 + 18 + 2 + 12),
-		numItems: 30,
-	},
-	// Stoner weapon.
-	{
-		address: 0x8f00 + 4 * 8 * (12 + 18 + 2 + 12 + 30),
-		numItems: 3,
-	},
-	// Willy Whistle/Drunk weapon + Super Socket/Invader weapon.
-	{
-		address: 0x8f00 + 4 * 8 * (12 + 18 + 2 + 12 + 30 + 3),
-		numItems: 10,
-		withMask: true,
-	},
-	// Incendo weapon.
-	{
-		address: 0x8f00 + 4 * 8 * (12 + 18 + 2 + 12 + 30 + 3 + 10),
-		numItems: 8,
-		withMask: true,
-	},
-	// Items
-	{
-		address: 0x8f00 + 4 * 8 * (12 + 18 + 2 + 12 + 30 + 3 + 10 + 8),
-		numItems: 58,
-	},
-	// Large lightning. (4x4 chars, but only 12 chars are stored.)
-	{
-		address: 0x8f00 + 4 * 8 * (12 + 18 + 2 + 12 + 30 + 3 + 10 + 9 + 57),
-		numItems: 6,
-		withMask: true,
-	},
-	// Bonus round circles.
-	{
-		address: 0x8f00 + 4 * 8 * (12 + 18 + 2 + 12 + 30 + 3 + 10 + 9 + 57 + 6),
-		numItems: 2,
-	},
-];
 
 export function parsePrg(prg: DataView): {
 	levels: Level[];
---
+++ src/prg/data-locations.ts
@@ -0,0 +1,83 @@
+export const platformCharArrayAddress = 0xc26e;
+export const sidebarCharArrayAddress = 0xbb0e;
+export const bgColorMetadataArrayAddress = 0xff30;
+export const holeMetadataArrayAddress = 0xc58e;
+export const symmetryMetadataArrayAddress = 0xff94;
+export const bitmapArrayAddress = 0xc5f2;
+export const monsterArrayAddress = 0xae51;
+export const windCurrentsArrayAddress = 0xb695;
+export const spriteBitmapArrayAddress = 0x5800;
+export const itemCharsArrays = [
+	// Blow bubble animation.
+	{
+		address: 0x8000,
+		numItems: 144 / 4,
+		withMask: true,
+	},
+	// Bubble pop.2 frames + position.
+	{
+		address: 0x8980,
+		numItems: 12,
+		withMask: true,
+	},
+	// Baron von Blubba.
+	{
+		address: 0x8f00,
+		numItems: 12,
+	},
+	// Special bubbles: Water, fire, lightning.
+	{
+		address: 0x8f00 + 4 * 8 * 12,
+		numItems: 18,
+	},
+	// Lightning
+	{
+		address: 0x8f00 + 4 * 8 * (12 + 18),
+		numItems: 2,
+		withMask: true,
+	},
+	// Fire
+	{
+		address: 0x8f00 + 4 * 8 * (12 + 18 + 2),
+		numItems: 12,
+		withMask: true,
+	},
+	// E-X-T-N-D bubbles.
+	{
+		address: 0x8f00 + 4 * 8 * (12 + 18 + 2 + 12),
+		numItems: 30,
+	},
+	// Stoner weapon.
+	{
+		address: 0x8f00 + 4 * 8 * (12 + 18 + 2 + 12 + 30),
+		numItems: 3,
+	},
+	// Willy Whistle/Drunk weapon + Super Socket/Invader weapon.
+	{
+		address: 0x8f00 + 4 * 8 * (12 + 18 + 2 + 12 + 30 + 3),
+		numItems: 10,
+		withMask: true,
+	},
+	// Incendo weapon.
+	{
+		address: 0x8f00 + 4 * 8 * (12 + 18 + 2 + 12 + 30 + 3 + 10),
+		numItems: 8,
+		withMask: true,
+	},
+	// Items
+	{
+		address: 0x8f00 + 4 * 8 * (12 + 18 + 2 + 12 + 30 + 3 + 10 + 8),
+		numItems: 58,
+	},
+	// Large lightning. (4x4 chars, but only 12 chars are stored.)
+	{
+		address: 0x8f00 + 4 * 8 * (12 + 18 + 2 + 12 + 30 + 3 + 10 + 9 + 57),
+		numItems: 6,
+		withMask: true,
+	},
+	// Bonus round circles.
+	{
+		address: 0x8f00 + 4 * 8 * (12 + 18 + 2 + 12 + 30 + 3 + 10 + 9 + 57 + 6),
+		numItems: 2,
+	},
+];
