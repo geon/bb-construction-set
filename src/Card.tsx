b9ee3176bd85962df331f3313e92b601822ecf26
Victor Widell <victor.widell@gmail.com>
2025-02-05 20:17:41 +0100

    Made colors work with dark mode.

--- src/Card.tsx
+++ src/Card.tsx
@@ -1,7 +1,11 @@
 import styled from "styled-components";
 
 export const Card = styled.div`
-	background: white;
+	background: hsl(0deg 0% 17%);
+	@media (prefers-color-scheme: light) {
+		background: white;
+	}
+
 	box-shadow: 0px 2px 5px #00000066;
 	border-radius: 5px;
 
