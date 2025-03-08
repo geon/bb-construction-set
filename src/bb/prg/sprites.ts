171c1561a5853738641a4c7ead44d0a071e51d1c
Victor Widell <victor.widell@gmail.com>
2025-03-08 12:15:58 +0100

    Added function readSpritesBin.

--- src/bb/prg/sprites.ts
+++ src/bb/prg/sprites.ts
@@ -1,13 +1,14 @@
-import { strictChunk } from "../functions";
+import { range, strictChunk } from "../functions";
 import {
 	Sprites,
 	characterNames,
 	CharacterName,
 	spriteCounts,
 	Sprite,
+	spriteColors,
 } from "../sprite";
 import { SpriteDataSegmentName } from "./data-locations";
-import { DataSegment } from "./io";
+import { DataSegment, uint8ArrayConcatenate } from "./io";
 
 export function readSprites(
 	dataSegments: Record<SpriteDataSegmentName, DataSegment>
@@ -37,4 +38,36 @@ export function readSprites(
 		sprites[characterName].push(sprite);
 	}
 	return sprites;
+}
+
+export function readSpritesBin(
+	dataSegments: Record<SpriteDataSegmentName, DataSegment>
+): Uint8Array {
+	const characterSpriteColors = characterNames
+		.map((name) => ({
+			count: spriteCounts[name],
+			color: spriteColors[name],
+		}))
+		.map((charcater) => Array<number>(charcater.count).fill(charcater.color))
+		.flat();
+
+	return uint8ArrayConcatenate(
+		(
+			Object.entries(dataSegments) as [SpriteDataSegmentName, DataSegment][]
+		).map(([segmentName, segment]) => {
+			const bufferCopy = new Uint8Array(segment.buffer).slice();
+
+			for (const spriteIndex of range(0, bufferCopy.length / 64)) {
+				const paddingByteIndex = (spriteIndex + 1) * 64 - 1;
+				const multicolorBit = 0b10000000;
+				const color =
+					segmentName !== "characters"
+						? spriteColors.player
+						: characterSpriteColors[spriteIndex];
+				bufferCopy[paddingByteIndex] = multicolorBit | color;
+			}
+
+			return bufferCopy;
+		})
+	);
 }
--- src/bb/tests/sprite.test.ts
+++ src/bb/tests/sprite.test.ts
@@ -1,5 +1,5 @@
 import { expect, test } from "vitest";
-import { readSprites } from "../prg/sprites";
+import { readSprites, readSpritesBin } from "../prg/sprites";
 import { getDataSegments } from "../prg/io";
 import { readFileSync } from "fs";
 import { spriteDataSegmentLocations } from "../prg/data-locations";
@@ -8,6 +8,14 @@ test("readSprites snapshot", () => {
 	const prg = readFileSync(__dirname + "/decompressed-bb.prg").buffer;
 	const segments = getDataSegments(prg, spriteDataSegmentLocations);
 	const sprites = readSprites(segments);
+
+	expect(sprites).toMatchSnapshot();
+});
+
+test("readSpritesBin snapshot", () => {
+	const prg = readFileSync(__dirname + "/decompressed-bb.prg").buffer;
+	const segments = getDataSegments(prg, spriteDataSegmentLocations);
+	const sprites = readSpritesBin(segments);
 
 	expect(sprites).toMatchSnapshot();
 });
