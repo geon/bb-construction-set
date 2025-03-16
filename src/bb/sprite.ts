7cb86c1bf8f33c62a9a42a83aaea0a15df696a79
Victor Widell <victor.widell@gmail.com>
2025-03-16 19:45:08 +0100

    Refactor. Hardcoded sprite colors should not be used.

--- src/bb/sprite.ts
+++ src/bb/sprite.ts
@@ -59,14 +59,14 @@ export const spriteLeftIndex: Record<CharacterName, number> = {
 	superSocket: 1,
 };
 
-export const spriteColors: Record<CharacterName, PaletteIndex> = {
+export const spriteColors: Record<"player", PaletteIndex> = {
 	player: 5,
-	bubbleBuster: 12,
-	incendo: 15,
-	colley: 5,
-	hullaballoon: 13,
-	beluga: 4,
-	willyWhistle: 5,
-	stoner: 3,
-	superSocket: 15,
+	// bubbleBuster: 12,
+	// incendo: 15,
+	// colley: 5,
+	// hullaballoon: 13,
+	// beluga: 4,
+	// willyWhistle: 5,
+	// stoner: 3,
+	// superSocket: 15,
 };
