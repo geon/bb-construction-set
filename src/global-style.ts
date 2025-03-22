fc20039c624fcc8f15f3bf4386cb8893df3b5e01
Victor Widell <victor.widell@gmail.com>
2025-03-22 22:25:49 +0100

    Revert "Refactor. Moved the #root element out of index.html."
    This reverts commit a7d34d847a75e6e66b4b94df0e9a28710fdc255c.

--- src/global-style.ts
+++ src/global-style.ts
@@ -1,6 +1,11 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
+	#root {
+		width: 100%;
+		height: 100%;
+	}
+
 	:root {
 		font-family: Inter, system-ui, Avenir, Helvetica, Arial, sans-serif;
 		line-height: 1.5;
