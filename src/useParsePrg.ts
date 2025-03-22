5277000a71b5e94fbb3851c0a7f1afdb31b68530
Victor Widell <victor.widell@gmail.com>
2025-03-22 10:43:09 +0100

    Refactor. Fixed typing.

--- src/useParsePrg.ts
+++ src/useParsePrg.ts
@@ -1,19 +1,11 @@
 import { useEffect, useState } from "react";
-import { CharBlock } from "./bb/charset-char";
-import { Level } from "./bb/level";
 import { parsePrg } from "./bb/parse-prg";
-import { Sprites } from "./bb/sprite";
-import { ItemDataSegmentName } from "./bb/prg/data-locations";
 import { attempt } from "./bb/functions";
 
 export type ParsePrgResult =
 	| {
 			readonly type: "ok";
-			readonly result: {
-				readonly levels: readonly Level[];
-				readonly sprites: Sprites;
-				readonly items: Record<ItemDataSegmentName, CharBlock[]>;
-			};
+			readonly result: ReturnType<typeof parsePrg>;
 	  }
 	| {
 			readonly type: "error";
