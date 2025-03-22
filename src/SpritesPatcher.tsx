d651deee09d753e36a65ca414fe4537b99160c69
Victor Widell <victor.widell@gmail.com>
2025-03-22 20:43:37 +0100

    Refactor. Joined SpriteBinSelector.tsx with SpritesPatcher.tsx.

--- src/SpritesPatcher.tsx
+++ src/SpritesPatcher.tsx
@@ -1,17 +1,34 @@
 import { ReactNode } from "react";
 import { patchPrgSpritesBin } from "./bb/parse-prg";
 import { BlobDownloadButton } from "./BlobDownloadButton";
-import { ParseSpriteBinResult } from "./useParseSpriteBin";
+import { useParseSpriteBin } from "./useParseSpriteBin";
+import { FileInput } from "./FileInput";
 
 export function SpritesPatcher({
 	prg,
-	parsedSpriteBinData,
 }: {
 	readonly prg: ArrayBuffer;
-	readonly parsedSpriteBinData: ParseSpriteBinResult | undefined;
 }): ReactNode {
+	const [parsedSpriteBinData, setSpriteBin] = useParseSpriteBin();
+
 	return (
 		<>
+			<h2>Select a SpritePad bin-file</h2>
+			<p>
+				Save the file generated above, then edit it in SpritePad, save it and
+				select it here.
+			</p>
+			<FileInput accept={["bin"]} onChange={setSpriteBin}>
+				Choose file
+			</FileInput>
+			{!parsedSpriteBinData ? (
+				<p>No bin selected.</p>
+			) : parsedSpriteBinData?.type !== "ok" ? (
+				<p>Could not parse bin: {parsedSpriteBinData?.error ?? "No reason."}</p>
+			) : (
+				<></>
+			)}
+
 			<h2>Patch</h2>
 			{!parsedSpriteBinData ? (
 				<p>Select both a prg and a pe file.</p>
