b7426dc250ae4bb2416e5d6af50d78e1c91de25c
Victor Widell <victor.widell@gmail.com>
2025-03-22 20:35:53 +0100

    Refactor. Fixed name.

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import { LevelsPatcher } from "./LevelsPatcher";
 import { PeSelector } from "./PeSelector";
 import { useParsePe } from "./useParsePe";
 import { SpriteBinPatchDownloader } from "./SpriteBinPatchDownloader";
-import { SpriteBinPrgSelector } from "./SpriteBinPrgSelector";
+import { SpritesVisualizerWithPeDownload } from "./SpritesVisualizerWithPeDownload";
 import { SpriteBinSelector } from "./SpriteBinSelector";
 import { useParseSpriteBin } from "./useParseSpriteBin";
 
@@ -69,7 +69,7 @@ export function App() {
 									<>
 										<Card>
 											<h2>{tab.title}</h2>
-											<SpriteBinPrgSelector prg={prg} />
+											<SpritesVisualizerWithPeDownload prg={prg} />
 										</Card>
 										<Card>
 											<SpriteBinSelector
