7f34e402b16e74c682c61d56714bbaec3d7c2fb2
Victor Widell <victor.widell@gmail.com>
2025-03-22 22:19:37 +0100

    Added Shallan prg link.

--- src/MinimalPrgSelector.tsx
+++ src/MinimalPrgSelector.tsx
@@ -14,6 +14,13 @@ export function MinimalPrgSelector({
 				prg-files you find will be <i>packed</i> and the c64 unpacks them on
 				startup. Such packed prg:s can't be used.
 			</p>
+			<p>
+				We recommend starting with the{" "}
+				<a href="https://github.com/smnjameson/letsdissectagame/raw/refs/heads/master/Episode%2011%20-%20Bubble%20Bobble/bin/crack.prg">
+					Shimmer bugfix by Shallan
+				</a>
+				.
+			</p>
 			<FileInput
 				accept={["prg"]}
 				onChange={async (file) => setPrg(await file?.arrayBuffer())}
