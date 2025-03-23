7868414af344e59fef87038223796e8edc5d1542
Victor Widell <victor.widell@gmail.com>
2025-03-23 21:39:32 +0100

    insdpsdsdf

--- src/BlobDownloadButton.tsx
+++ src/BlobDownloadButton.tsx
@@ -1,14 +1,29 @@
 export function BlobDownloadButton(props: {
 	getBlob: () => {
-		readonly blob: Blob;
 		readonly fileName: string;
-	};
+	} & (
+		| {
+				readonly blob: Blob;
+		  }
+		| {
+				readonly parts: {
+					readonly fileName: string;
+					readonly blob: Blob;
+				};
+		  }
+	);
 	label: string;
 }) {
 	return (
 		<button
-			onClick={() => {
-				const { blob, fileName } = props.getBlob();
+			onClick={async () => {
+				const result = props.getBlob();
+				const { blob, fileName } = !("parts" in result)
+					? result
+					: {
+							blob: await new Response(makeZip(result.blobs)).blob(),
+							fileName: result.fileName,
+					  };
 
 				var link = document.createElement("a");
 				link.download = fileName;
