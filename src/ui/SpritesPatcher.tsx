2497e373dd06b58240531d34517756744cf5ec25
Victor Widell <victor.widell@gmail.com>
2025-04-12 23:54:24 +0200

    Apply the patch at once on successful parse. Sprites.

--- src/ui/SpritesPatcher.tsx
+++ src/ui/SpritesPatcher.tsx
@@ -1,7 +1,7 @@
-import { ReactNode, useState } from "react";
+import { ReactNode } from "react";
 import { patchPrgSpritesBin } from "../bb/prg/parse-prg";
 import { FileInput } from "./FileInput";
-import { Attempt, attempt } from "../bb/functions";
+import { attempt } from "../bb/functions";
 import { parseSpriteGroupsFromBin } from "../bb/prg/sprites";
 
 export function SpritesPatcher({
@@ -11,19 +11,6 @@ export function SpritesPatcher({
 	readonly prg: ArrayBuffer;
 	readonly setPrg: (file: ArrayBuffer | undefined) => void;
 }): ReactNode {
-	const [parsedSpriteBinData, setParsedSpriteBinData] = useState<
-		Attempt<ReturnType<typeof parseSpriteGroupsFromBin>> | undefined
-	>(undefined);
-
-	const setSpriteBin = (buffer: ArrayBuffer | undefined) =>
-		setParsedSpriteBinData(
-			buffer &&
-				attempt(() => {
-					const parsed = parseSpriteGroupsFromBin(new Uint8Array(buffer));
-					return parsed;
-				})
-		);
-
 	return (
 		<>
 			<p>
@@ -32,32 +19,31 @@ export function SpritesPatcher({
 			</p>
 			<FileInput
 				accept={["bin"]}
-				onChange={async (file) => setSpriteBin(await file?.arrayBuffer())}
+				onChange={async (file) => {
+					const buffer = await file?.arrayBuffer();
+
+					const parsedSpriteBinData =
+						buffer &&
+						attempt(() => {
+							const parsed = parseSpriteGroupsFromBin(new Uint8Array(buffer));
+							return parsed;
+						});
+
+					if (parsedSpriteBinData?.type !== "ok") {
+						alert(
+							`Could not parse bin: ${
+								parsedSpriteBinData?.error ?? "No reason."
+							}`
+						);
+						return;
+					}
+
+					const patched = patchPrgSpritesBin(prg, parsedSpriteBinData.result);
+					setPrg(patched);
+				}}
 			>
 				Choose file
 			</FileInput>
-			<br />
-			<br />
-			{!parsedSpriteBinData ? (
-				<p>No bin selected.</p>
-			) : parsedSpriteBinData?.type !== "ok" ? (
-				<p>Could not parse bin: {parsedSpriteBinData?.error ?? "No reason."}</p>
-			) : (
-				<>
-					<button
-						onClick={() => {
-							const patched = patchPrgSpritesBin(
-								prg,
-								parsedSpriteBinData.result
-							);
-							setPrg(patched);
-							setSpriteBin(undefined);
-						}}
-					>
-						Apply Patch
-					</button>
-				</>
-			)}
 		</>
 	);
 }
