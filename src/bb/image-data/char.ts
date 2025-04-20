de12c0fe3cb0bec266888a8d7c423cbae41ac4a5
Victor Widell <victor.widell@gmail.com>
2025-04-20 16:19:10 +0200

    Refactor. Moved function to new file.

---
+++ src/bb/image-data/char.ts
@@ -0,0 +1,28 @@
+import { Color } from "../../math/color";
+import { CharsetChar } from "../internal-data-formats/charset-char";
+import { plotPixel } from "./image-data";
+
+export function drawChar(
+	char: CharsetChar,
+	charPalette: readonly [Color, Color, Color, Color],
+	mask?: CharsetChar
+) {
+	const image = new ImageData(8, 8);
+
+	for (const [charY, line] of char.lines.entries()) {
+		for (const [charX, colorIndex] of line.entries()) {
+			const masked = mask?.lines?.[charY]?.[charX];
+			if (masked !== undefined && !(masked === 0b11 || masked === 0b00)) {
+				throw new Error("Invalid mask pixel");
+			}
+			const alpha = masked ? 0 : 255;
+
+			const color = charPalette[colorIndex];
+			// Double width pixels.
+			const pixelIndex = charY * 8 + charX * 2;
+			plotPixel(image, pixelIndex, color, alpha);
+			plotPixel(image, pixelIndex + 1, color, alpha);
+		}
+	}
+	return image;
+}
--- src/bb/image-data/draw-levels-to-canvas.ts
+++ src/bb/image-data/draw-levels-to-canvas.ts
@@ -34,6 +34,7 @@ import {
 	drawGrid,
 	plotPixel,
 } from "./image-data";
+import { drawChar } from "./char";
 
 export function drawLevelsToCanvas(
 	levels: readonly Level[],
@@ -185,31 +186,6 @@ function getCharPalette(level: Level): [Color, Color, Color, Color] {
 		// The color ram gets cleared to green at the beginning of the game.
 		palette[5],
 	];
-}
-
-function drawChar(
-	char: CharsetChar,
-	charPalette: readonly [Color, Color, Color, Color],
-	mask?: CharsetChar
-) {
-	const image = new ImageData(8, 8);
-
-	for (const [charY, line] of char.lines.entries()) {
-		for (const [charX, colorIndex] of line.entries()) {
-			const masked = mask?.lines?.[charY]?.[charX];
-			if (masked !== undefined && !(masked === 0b11 || masked === 0b00)) {
-				throw new Error("Invalid mask pixel");
-			}
-			const alpha = masked ? 0 : 255;
-
-			const color = charPalette[colorIndex];
-			// Double width pixels.
-			const pixelIndex = charY * 8 + charX * 2;
-			plotPixel(image, pixelIndex, color, alpha);
-			plotPixel(image, pixelIndex + 1, color, alpha);
-		}
-	}
-	return image;
 }
 
 export function layOutSpriteGroups(): LayoutRect {
