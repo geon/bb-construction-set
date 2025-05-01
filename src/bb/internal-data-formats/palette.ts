f26bf2f560aa4aa35ecada84aa1b1a59a314601d
Victor Widell <victor.widell@gmail.com>
2025-05-01 10:39:24 +0200

    Refactor. Cleanup.

--- src/bb/internal-data-formats/palette.ts
+++ src/bb/internal-data-formats/palette.ts
@@ -1,7 +1,7 @@
-import { Color, hexToRgb } from "../../math/color";
-import { ReadonlyTuple, Tuple } from "../tuple";
+import { hexToRgb } from "../../math/color";
+import { mapTuple, ReadonlyTuple } from "../tuple";
 
-type Palette = Tuple<Color, 16>;
+// type Palette = ReadonlyTuple<Color, 16>;
 export type PaletteIndex =
 	| 0
 	| 1
@@ -20,13 +20,16 @@ export type PaletteIndex =
 	| 14
 	| 15;
 
-// https://www.pepto.de/projects/colorvic/2001
-// https://www.c64-wiki.com/wiki/Color
-export const palette = [
-	0x000000, 0xffffff, 0x68372b, 0x70a4b2, 0x6f3d86, 0x588d43, 0x352879,
-	0xb8c76f, 0x6f4f25, 0x433900, 0x9a6759, 0x444444, 0x6c6c6c, 0x9ad284,
-	0x6c5eb5, 0x959595,
-].map(hexToRgb) as Palette;
+export const palette = mapTuple(
+	// https://www.pepto.de/projects/colorvic/2001
+	// https://www.c64-wiki.com/wiki/Color
+	[
+		0x000000, 0xffffff, 0x68372b, 0x70a4b2, 0x6f3d86, 0x588d43, 0x352879,
+		0xb8c76f, 0x6f4f25, 0x433900, 0x9a6759, 0x444444, 0x6c6c6c, 0x9ad284,
+		0x6c5eb5, 0x959595,
+	],
+	hexToRgb
+);
 
 export type SubPalette = ReadonlyTuple<PaletteIndex, 4>;
 export type SubPaletteIndex = 0 | 1 | 2 | 3;
