d7824d33c83aa63747bf90723f0fab159cf109c3
Victor Widell <victor.widell@gmail.com>
2025-07-06 22:29:45 +0200

    Refactor. Moved levels parsing/patch generation to separate file.

---
+++ src/bb/prg/levels.ts
@@ -0,0 +1,85 @@
+import { zipObject, unzipObject, objectEntries } from "../functions";
+import { LevelDataSegmentName } from "../game-definitions/level-segment-name";
+import { Level } from "../internal-data-formats/level";
+import { readBgColors, writeBgColors } from "./bg-colors";
+import { readBubbleCurrentPerLineDefaults } from "./bubble-current-per-line-defaults";
+import {
+	readBubbleCurrentRectangles,
+	writeBubbleCurrentRectangles,
+} from "./bubble-current-rectangles";
+import { readPlatformChars, writePlatformChars } from "./charset-char";
+import { levelSegmentLocations } from "./data-locations";
+import { DataSegment, patchFromSegment } from "./io";
+import { writeHoles, writeSymmetry, writeBitmaps } from "./misc-patch";
+import { readMonsters, writeMonsters as getPatchMonsters } from "./monsters";
+import {
+	readSidebarChars,
+	writeSidebarChars,
+	writeSidebarCharsIndex,
+} from "./sidebar-chars";
+import { readTileBitmaps } from "./tile-bitmap";
+import { readTiles } from "./tiles";
+
+export function readLevels(
+	dataSegments: Record<LevelDataSegmentName, DataSegment>
+): ReadonlyArray<Level> {
+	const tileBitmaps = readTileBitmaps(
+		dataSegments.bitmaps.buffer,
+		dataSegments.symmetry.buffer
+	);
+
+	return zipObject({
+		platformChar: readPlatformChars(dataSegments.platformChars.buffer),
+		...readBgColors(dataSegments.bgColors.buffer),
+		sidebarChars: readSidebarChars(
+			dataSegments.sidebarChars.buffer,
+			dataSegments.sidebarCharsIndex.buffer
+		),
+		tiles: readTiles(dataSegments.holeMetadata.buffer, tileBitmaps),
+		monsters: readMonsters(dataSegments.monsters.buffer),
+		bubbleCurrentRectangles: readBubbleCurrentRectangles(
+			dataSegments.windCurrents.buffer
+		),
+		bubbleCurrentPerLineDefaults: readBubbleCurrentPerLineDefaults(
+			dataSegments.holeMetadata.buffer,
+			tileBitmaps
+		),
+	});
+}
+
+export function getLevelsPatch(levels: readonly Level[]) {
+	if (levels.length !== 100) {
+		throw new Error(`Wrong number of levels: ${levels.length}. Should be 100.`);
+	}
+
+	const unzippedLevels = unzipObject(levels);
+
+	const newSegments = {
+		platformChars: writePlatformChars(unzippedLevels.platformChar),
+		sidebarChars: writeSidebarChars(unzippedLevels.sidebarChars),
+		bgColors: writeBgColors(
+			unzippedLevels.bgColorLight,
+			unzippedLevels.bgColorDark
+		),
+		holeMetadata: writeHoles(
+			unzippedLevels.tiles,
+			unzippedLevels.bubbleCurrentPerLineDefaults
+		),
+		symmetry: writeSymmetry(unzippedLevels.tiles),
+		sidebarCharsIndex: writeSidebarCharsIndex(unzippedLevels.sidebarChars),
+		bitmaps: writeBitmaps(
+			unzippedLevels.tiles,
+			unzippedLevels.bubbleCurrentPerLineDefaults
+		),
+		windCurrents: writeBubbleCurrentRectangles(
+			unzippedLevels.bubbleCurrentRectangles
+		),
+	} as const;
+
+	return [
+		objectEntries(newSegments).flatMap(([segmentName, newSegment]) =>
+			patchFromSegment(levelSegmentLocations[segmentName], newSegment)
+		),
+		getPatchMonsters(unzippedLevels.monsters),
+	].flat();
+}
--- src/bb/prg/parse-prg.ts
+++ src/bb/prg/parse-prg.ts
@@ -1,14 +1,5 @@
-import { mapRecord, objectEntries, unzipObject, zipObject } from "../functions";
-import { Level } from "../internal-data-formats/level";
-import { writeBgColors, readBgColors } from "./bg-colors";
-import { readPlatformChars, writePlatformChars } from "./charset-char";
-import {
-	getDataSegments,
-	DataSegment,
-	getDataSegment,
-	patchFromSegment,
-	applyPatch,
-} from "./io";
+import { mapRecord } from "../functions";
+import { getDataSegments, getDataSegment, applyPatch } from "./io";
 import {
 	charSegmentLocations,
 	itemSegmentLocations,
@@ -17,29 +8,15 @@ import {
 	monsterSpriteColorsSegmentLocation,
 	spriteDataSegmentLocations,
 } from "./data-locations";
-import { LevelDataSegmentName } from "../game-definitions/level-segment-name";
 import { getCharGroupsPatch, parseCharGroups } from "./char-groups";
 import { getItemPatch as getItemsPatch, parseItems } from "./items";
-import {
-	readBubbleCurrentRectangles,
-	writeBubbleCurrentRectangles,
-} from "./bubble-current-rectangles";
-import {
-	writeSidebarChars,
-	readSidebarChars,
-	writeSidebarCharsIndex,
-} from "./sidebar-chars";
-import { readTiles } from "./tiles";
-import { writeMonsters as getPatchMonsters, readMonsters } from "./monsters";
 import {
 	getSpriteColorsPatch,
 	getSpritesPatch,
 	parseSpriteGroupsFromPrg,
 } from "./sprites";
-import { readTileBitmaps } from "./tile-bitmap";
-import { writeSymmetry, writeBitmaps, writeHoles } from "./misc-patch";
-import { readBubbleCurrentPerLineDefaults } from "./bubble-current-per-line-defaults";
 import { ParsedPrg } from "../internal-data-formats/parsed-prg";
+import { readLevels, getLevelsPatch } from "./levels";
 
 export function parsePrg(prg: ArrayBuffer): ParsedPrg {
 	const levels = readLevels(getDataSegments(prg, levelSegmentLocations));
@@ -56,70 +33,6 @@ export function parsePrg(prg: ArrayBuffer): ParsedPrg {
 	);
 
 	return { levels, sprites, chars, items };
-}
-
-function readLevels(
-	dataSegments: Record<LevelDataSegmentName, DataSegment>
-): ReadonlyArray<Level> {
-	const tileBitmaps = readTileBitmaps(
-		dataSegments.bitmaps.buffer,
-		dataSegments.symmetry.buffer
-	);
-
-	return zipObject({
-		platformChar: readPlatformChars(dataSegments.platformChars.buffer),
-		...readBgColors(dataSegments.bgColors.buffer),
-		sidebarChars: readSidebarChars(
-			dataSegments.sidebarChars.buffer,
-			dataSegments.sidebarCharsIndex.buffer
-		),
-		tiles: readTiles(dataSegments.holeMetadata.buffer, tileBitmaps),
-		monsters: readMonsters(dataSegments.monsters.buffer),
-		bubbleCurrentRectangles: readBubbleCurrentRectangles(
-			dataSegments.windCurrents.buffer
-		),
-		bubbleCurrentPerLineDefaults: readBubbleCurrentPerLineDefaults(
-			dataSegments.holeMetadata.buffer,
-			tileBitmaps
-		),
-	});
-}
-
-export function getLevelsPatch(levels: readonly Level[]) {
-	if (levels.length !== 100) {
-		throw new Error(`Wrong number of levels: ${levels.length}. Should be 100.`);
-	}
-
-	const unzippedLevels = unzipObject(levels);
-
-	const newSegments = {
-		platformChars: writePlatformChars(unzippedLevels.platformChar),
-		sidebarChars: writeSidebarChars(unzippedLevels.sidebarChars),
-		bgColors: writeBgColors(
-			unzippedLevels.bgColorLight,
-			unzippedLevels.bgColorDark
-		),
-		holeMetadata: writeHoles(
-			unzippedLevels.tiles,
-			unzippedLevels.bubbleCurrentPerLineDefaults
-		),
-		symmetry: writeSymmetry(unzippedLevels.tiles),
-		sidebarCharsIndex: writeSidebarCharsIndex(unzippedLevels.sidebarChars),
-		bitmaps: writeBitmaps(
-			unzippedLevels.tiles,
-			unzippedLevels.bubbleCurrentPerLineDefaults
-		),
-		windCurrents: writeBubbleCurrentRectangles(
-			unzippedLevels.bubbleCurrentRectangles
-		),
-	} as const;
-
-	return [
-		objectEntries(newSegments).flatMap(([segmentName, newSegment]) =>
-			patchFromSegment(levelSegmentLocations[segmentName], newSegment)
-		),
-		getPatchMonsters(unzippedLevels.monsters),
-	].flat();
 }
 
 export function patchPrg(prg: ArrayBuffer, parsedPrg: ParsedPrg): ArrayBuffer {
