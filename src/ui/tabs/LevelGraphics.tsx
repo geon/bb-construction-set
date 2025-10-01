7dd0b95f152428ac46b8aa9603c37ff2f19c4995
Victor Widell <victor.widell@gmail.com>
2025-10-01 13:55:44 +0200

    Removed pe-file support. Using image import/export instead.

--- src/ui/tabs/LevelGraphics.tsx
+++ src/ui/tabs/LevelGraphics.tsx
@@ -1,21 +1,25 @@
 import { ReactNode } from "react";
-import {
-	levelsToPeFileData,
-	peFileDataToLevels,
-} from "../../bb/pe/level-pe-conversion";
-import {
-	deserializePeFileData,
-	serializePeFileData,
-} from "../../bb/pe/pe-file";
 import { BlobDownloadButton } from "../BlobDownloadButton";
-import { attempt, unzipObject, zipObject } from "../../bb/functions";
+import { attempt, zipObject } from "../../bb/functions";
 import { ParsedPrg } from "../../bb/internal-data-formats/parsed-prg";
-import { drawLevelPlatformChars } from "../../bb/palette-image/char";
+import {
+	drawLevelPlatformChars,
+	drawPlatformChars,
+	parsePlatformChars,
+} from "../../bb/palette-image/char";
 import { ImageDataCanvas } from "../ImageDataCanvas";
 import { FileInput } from "../FileInput";
-import { imageDataFromPaletteImage } from "../../bb/image-data/image-data";
-import { doubleImageWidth } from "../../bb/palette-image/palette-image";
-import { assertTuple } from "../../bb/tuple";
+import {
+	imageDataFromImage,
+	imageDataFromPaletteImage,
+	imageDataToBlob,
+	imageFromFile,
+	paletteImageFromImageData,
+} from "../../bb/image-data/image-data";
+import {
+	doubleImageWidth,
+	halfImageWidth,
+} from "../../bb/palette-image/palette-image";
 import styled from "styled-components";
 
 const Styling = styled.div`
@@ -50,79 +54,47 @@ export function LevelGraphics({
 			/>
 			<ImageButtons>
 				<FileInput
-					accept={["pe"]}
-					multiple
-					onChange={async (files) => {
-						const pes = await Promise.all(
-							files.map((file) => file.arrayBuffer())
+					accept={["image/*"]}
+					onChange={async (file) => {
+						const imageData = imageDataFromImage(await imageFromFile(file));
+
+						const parsedTiles = attempt(() =>
+							parsePlatformChars(
+								halfImageWidth(paletteImageFromImageData(imageData))
+							)
 						);
 
-						const parsedPeData =
-							pes &&
-							attempt(() => {
-								const deserializedPeFileDatas = pes.map((buffer) =>
-									deserializePeFileData(new TextDecoder("utf-8").decode(buffer))
-								);
-								const levels =
-									deserializedPeFileDatas.flatMap(peFileDataToLevels);
-
-								return {
-									levels,
-									deserializedPeFileDatas,
-								};
-							});
-
-						if (parsedPeData?.type !== "ok") {
+						if (parsedTiles.type !== "ok") {
 							alert(
-								`Could not parse pe: ${parsedPeData?.error ?? "No reason."}`
+								`Could not read image: ${parsedTiles.error ?? "No reason."}`
 							);
 							return;
 						}
 
-						const old = unzipObject(parsedPrg.levels);
-						const new_ = unzipObject(parsedPeData.result.levels);
-						const levelsWithNewGraphics = zipObject({
-							...old,
-							bgColors: new_.bgColors,
-							platformChar: new_.platformChar,
-							sidebarChars: new_.sidebarChars,
+						setParsedPrg({
+							...parsedPrg,
+							levels: zipObject({
+								level: parsedPrg.levels,
+								platformCharsData: parsedTiles.result,
+							}).map(({ level, platformCharsData }) => ({
+								...level,
+								...platformCharsData,
+							})),
 						});
-
-						setParsedPrg({ ...parsedPrg, levels: levelsWithNewGraphics });
 					}}
 				>
-					Choose files
+					Import Image
 				</FileInput>
 				<BlobDownloadButton
-					getBlob={async () => {
-						const parts = parsedPrg.levels.map((level, index) => {
-							const blob = new Blob(
-								[
-									serializePeFileData(
-										levelsToPeFileData({
-											...parsedPrg,
-											levels: [level],
-											shadowChars: assertTuple(
-												parsedPrg.chars.shadows.flat().flat(),
-												6
-											),
-										})
-									),
-								],
-								{ type: "application/json" }
-							);
-
-							const fileName = (index + 1).toString().padStart(3, "0") + ".pe";
-
-							return { blob, fileName };
-						});
-
-						return {
-							parts,
-							fileName: "bubble bobble c64 - all levels.zip",
-						};
-					}}
-					label="Download PETSCII Editor file"
+					getBlob={async () => ({
+						fileName: "level graphics.png",
+						blob: await imageDataToBlob(
+							imageDataFromPaletteImage(
+								doubleImageWidth(drawPlatformChars(parsedPrg.levels))
+							)
+						),
+					})}
+					label={"Export Image"}
 				/>
 			</ImageButtons>
 		</Styling>
