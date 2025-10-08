3b5cb2ad2cd65ef2779d3e50ae2d7386cdd9b899
Victor Widell <victor.widell@gmail.com>
2025-10-08 21:51:42 +0200

    Changed argument of setTile to an array.

--- src/bb/game-definitions/level-size.ts
+++ src/bb/game-definitions/level-size.ts
@@ -3,7 +3,7 @@ import { Coord2 } from "../../math/coord2";
 export const levelWidth = 32;
 export const levelHeight = 25;
 
-export const levelSize: Coord2 = {
+export const levelSize = {
 	x: levelWidth,
 	y: levelHeight,
-};
+} as const satisfies Coord2;
