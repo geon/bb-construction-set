54415f6e3e2f5560b6c70f3a2aeb1d2b04dbd78f
Victor Widell <victor.widell@gmail.com>
2025-10-08 22:42:14 +0200

    Refactor. Moved event handlers to object.

--- src/ui/tabs/PlatformEditor.tsx
+++ src/ui/tabs/PlatformEditor.tsx
@@ -5,46 +5,54 @@ import { Coord2 } from "../../math/coord2";
 import { levelSize } from "../../bb/game-definitions/level-size";
 import { imageDataFromPaletteImage } from "../../bb/image-data/image-data";
 import { drawLevelTiles } from "../../bb/palette-image/level";
-import { ClickDragCanvas } from "../ClickDragCanvas";
+import {
+	ClickDragCanvas,
+	ClickDragCanvasDragEventHandlers,
+} from "../ClickDragCanvas";
 import { bresenham } from "../../bb/functions";
 
 export function PlatformEditor(props: {
 	readonly tiles: Tiles;
 	readonly setTiles: (tiles: Tiles) => void;
 }): JSX.Element {
+	const { tiles } = props;
 	const setSomeTiles = createSetSomeTiles(props.setTiles, props.tiles);
 	let [drawValue, setDrawValue] = useState<boolean | undefined>(undefined);
 	let [lineStart, setLineStart] = useState<Coord2 | undefined>(undefined);
+
+	const eventHandlers: ClickDragCanvasDragEventHandlers = {
+		onClick: (tileCoord) => {
+			if (drawValue === undefined) {
+				return;
+			}
+			setSomeTiles([tileCoord], drawValue);
+		},
+		onDragStart: (tileCoord) => {
+			const newDrawValue = !tiles[tileCoord.y]![tileCoord.x]!;
+			setDrawValue(newDrawValue);
+			setLineStart(tileCoord);
+		},
+		onDragEnd: () => {
+			setDrawValue(undefined);
+			setLineStart(undefined);
+		},
+		onDragUpdate: (tileCoord) => {
+			if (drawValue === undefined) {
+				return;
+			}
+			if (lineStart === undefined) {
+				return;
+			}
+			setSomeTiles(bresenham(lineStart, tileCoord), drawValue);
+			setLineStart(tileCoord);
+		},
+	};
 
 	return (
 		<ClickDragCanvas
 			style={{ width: "100%" }}
 			imageData={imageDataFromPaletteImage(drawLevelTiles(props.tiles))}
-			onClick={(tileCoord) => {
-				if (drawValue === undefined) {
-					return;
-				}
-				setSomeTiles([tileCoord], drawValue);
-			}}
-			onDragStart={(tileCoord) => {
-				const newDrawValue = !props.tiles[tileCoord.y]![tileCoord.x]!;
-				setDrawValue(newDrawValue);
-				setLineStart(tileCoord);
-			}}
-			onDragEnd={() => {
-				setDrawValue(undefined);
-				setLineStart(undefined);
-			}}
-			onDragUpdate={(tileCoord) => {
-				if (drawValue === undefined) {
-					return;
-				}
-				if (lineStart === undefined) {
-					return;
-				}
-				setSomeTiles(bresenham(lineStart, tileCoord), drawValue);
-				setLineStart(tileCoord);
-			}}
+			{...eventHandlers}
 		/>
 	);
 }
