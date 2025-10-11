36ddbc27ad78715a557eae353f3d927e82da84c6
Victor Widell <victor.widell@gmail.com>
2025-10-11 15:07:25 +0200

    Refactor. Moved Palette to new file.

--- src/ui/tabs/Items.tsx
+++ src/ui/tabs/Items.tsx
@@ -5,10 +5,7 @@ import {
 	CharGroup,
 } from "../../bb/internal-data-formats/char-group";
 import styled from "styled-components";
-import {
-	rgbPalette,
-	PaletteIndex,
-} from "../../bb/internal-data-formats/palette";
+import { PaletteIndex } from "../../bb/internal-data-formats/palette";
 import {
 	checkedAccess,
 	range,
@@ -24,58 +21,7 @@ import { TabBar } from "../TabBar";
 import { CharBlockSelector } from "../CharBlockSelector";
 import { getCharPalette } from "../../bb/palette-image/char";
 import { itemNames } from "../../bb/game-definitions/item-names";
-
-const pickerSize = "2em";
-const PaletteIndexButton = styled.button.attrs<{ selected: boolean }>(
-	(props) => ({
-		className: props.selected ? "selected" : "",
-	})
-)<{
-	readonly $paletteIndex: PaletteIndex;
-}>`
-	width: ${pickerSize};
-	height: ${pickerSize};
-	border-radius: 10000px;
-	padding: 0;
-
-	background-color: ${({ $paletteIndex }) =>
-		`rgb(${Object.values(rgbPalette[$paletteIndex]).join(", ")})`};
-
-	&.selected {
-		box-shadow: 0 0 0 2px black, 0 0 0 3px white;
-		@media (prefers-color-scheme: light) {
-			box-shadow: 0 0 0 2px white, 0 0 0 3px black;
-		}
-	}
-`;
-const Palette = styled(
-	(props: {
-		readonly selectePaletteIndex: PaletteIndex;
-		readonly options: readonly PaletteIndex[];
-		readonly onPick: (paletteIndex: PaletteIndex) => void;
-		readonly className?: string;
-	}): JSX.Element => {
-		return (
-			<nav className={props.className}>
-				{props.options.map((index) => {
-					const paletteIndex = index as PaletteIndex;
-					return (
-						<PaletteIndexButton
-							key={paletteIndex}
-							$paletteIndex={paletteIndex}
-							selected={paletteIndex === props.selectePaletteIndex}
-							onClick={() => props.onPick(paletteIndex)}
-						/>
-					);
-				})}
-			</nav>
-		);
-	}
-)`
-	display: flex;
-	gap: 8px;
-	justify-content: center;
-`;
+import { Palette } from "../Palette";
 
 const Styling = styled.div`
 	display: flex;
