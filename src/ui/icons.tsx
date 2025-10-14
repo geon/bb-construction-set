b10dac7261f729dc9d329f2005d6127b869a6c85
Victor Widell <victor.widell@gmail.com>
2025-10-14 15:55:34 +0200

    fixup! Nicer +/- icons.

--- src/ui/icons.tsx
+++ src/ui/icons.tsx
@@ -134,10 +134,7 @@ export const icons = {
 			fill="currentColor"
 			viewBox="0 0 16 16"
 		>
-			<path
-				fill-rule="evenodd"
-				d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2"
-			/>
+			<path d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2" />
 		</svg>
 	),
 	// https://icons.getbootstrap.com/icons/dash-lg/
@@ -149,10 +146,7 @@ export const icons = {
 			fill="currentColor"
 			viewBox="0 0 16 16"
 		>
-			<path
-				fill-rule="evenodd"
-				d="M2 8a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11A.5.5 0 0 1 2 8"
-			/>
+			<path d="M2 8a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11A.5.5 0 0 1 2 8" />
 		</svg>
 	),
 };
