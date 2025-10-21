c50eb8acb02753322b90b6293018809c024c0ff6
Victor Widell <victor.widell@gmail.com>
2025-10-21 11:07:56 +0200

    Refactor. Fixed typing.

--- src/bb/prg/bg-colors.ts
+++ src/bb/prg/bg-colors.ts
@@ -1,16 +1,18 @@
 import { BgColors } from "../internal-data-formats/bg-colors";
 import { PaletteIndex } from "../internal-data-formats/palette";
+import { assertTuple, Tuple } from "../tuple";
 import { ReadonlyUint8Array } from "../types";
 
-export function readBgColors(
-	bytes: ReadonlyUint8Array
-): ReadonlyArray<BgColors> {
-	return [...bytes].map((bgColorMetadata) => ({
-		light: (bgColorMetadata & 0b1111) as PaletteIndex,
-		dark: ((bgColorMetadata & 0b11110000) >> 4) as PaletteIndex,
-	}));
+export function readBgColors(bytes: ReadonlyUint8Array): Tuple<BgColors, 100> {
+	return assertTuple(
+		[...bytes].map((bgColorMetadata) => ({
+			light: (bgColorMetadata & 0b1111) as PaletteIndex,
+			dark: ((bgColorMetadata & 0b11110000) >> 4) as PaletteIndex,
+		})),
+		100
+	);
 }
 
-export function writeBgColors(bgColors: ReadonlyArray<BgColors>): Uint8Array {
+export function writeBgColors(bgColors: Tuple<BgColors, 100>): Uint8Array {
 	return new Uint8Array(bgColors.map(({ light, dark }) => light + (dark << 4)));
 }
