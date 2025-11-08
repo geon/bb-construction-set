7b57812ef0ef4b1b1db027b438fe515033639c52
Victor Widell <victor.widell@gmail.com>
2025-11-08 18:03:08 +0100

    Refactor. Removed unused type.

--- src/bb/internal-data-formats/char.ts
+++ src/bb/internal-data-formats/char.ts
@@ -3,4 +3,3 @@ import { ColorPixelByte } from "./color-pixel-byte";
 
 export interface Char extends Tuple<ColorPixelByte, 8> {}
 export type CharBlock = Tuple<Char, 4>;
-export type CharBlockIndex = 0 | 1 | 2 | 3;
