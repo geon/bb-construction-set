98e1296253c76d983e41f001fdb67a0c71be5557
Victor Widell <victor.widell@gmail.com>
2025-11-17 21:35:21 +0100

    Added platform scrolling buttons.

--- src/ui/ClickDragCanvasEventHandlerProviders/DrawPlatforms.tsx
+++ src/ui/ClickDragCanvasEventHandlerProviders/DrawPlatforms.tsx
@@ -1,7 +1,7 @@
-import { objectEntries, range } from "../../bb/functions";
+import { mapRecord, objectEntries, range } from "../../bb/functions";
 import { Coord2, floor, scale } from "../../math/coord2";
 import { ClickDragCanvasEventHandlerProvider } from "../ClickDragCanvasEventHandlerProvider";
-import { assertTuple } from "../../bb/tuple";
+import { assertTuple, mapTuple } from "../../bb/tuple";
 import { levelSize } from "../../bb/game-definitions/level-size";
 import { rectContainsPoint } from "../../math/rect";
 import { holeRects } from "../../bb/game-definitions/holes";
@@ -11,6 +11,10 @@ import {
 	Tiles,
 } from "../../bb/internal-data-formats/tiles";
 import { useDraw } from "./use-draw";
+import { Level, platformTilesSize } from "../../bb/internal-data-formats/level";
+import { ButtonGroup } from "../ButtonGroup";
+import { ButtonRow } from "../ButtonRow";
+import { icons } from "../icons";
 
 const holes = objectEntries(holeRects).flatMap(([row, holes]) =>
 	objectEntries(holes).map(([side, hole]) => ({ row, side, hole }))
@@ -32,7 +36,92 @@ export const DrawPlatforms: ClickDragCanvasEventHandlerProvider = (props) => {
 	}
 
 	const transformCoord = getTileCoord;
-	return props.children(useDraw(getDrawValue, setSomeTiles, transformCoord));
+	return props.children(
+		useDraw(getDrawValue, setSomeTiles, transformCoord),
+		<ButtonRow $align="right">
+			<ButtonGroup>
+				<button
+					onClick={() =>
+						props.setLevel({
+							...level,
+							platformTiles: assertTuple(
+								level.platformTiles.slice().reverse(),
+								platformTilesSize.y
+							),
+							monsters: level.monsters.map((monster) => ({
+								...monster,
+								spawnPoint: {
+									...monster.spawnPoint,
+									y: levelSize.y * 8 - monster.spawnPoint.y + (42 + 4 * 8),
+								},
+							})),
+							itemSpawnPositions: mapRecord(level.itemSpawnPositions, (q) => ({
+								...q,
+								y: levelSize.y - q.y - 2,
+							})),
+							holes: {
+								top: level.holes.bottom,
+								bottom: level.holes.top,
+							},
+						})
+					}
+				>
+					{icons.verticalArrows}
+				</button>
+				<button
+					onClick={() =>
+						props.setLevel({
+							...level,
+							platformTiles: assertTuple(
+								level.platformTiles.map((row) =>
+									assertTuple(row.slice().reverse(), platformTilesSize.x)
+								),
+								platformTilesSize.y
+							),
+							monsters: level.monsters.map((monster) => ({
+								...monster,
+								spawnPoint: {
+									...monster.spawnPoint,
+									x: levelSize.x * 8 - monster.spawnPoint.x + (40 - 2 * 8),
+								},
+								facingLeft: !monster.facingLeft,
+							})),
+							itemSpawnPositions: mapRecord(level.itemSpawnPositions, (q) => ({
+								...q,
+								x: levelSize.x - q.x - 2,
+							})),
+							holes: {
+								top: {
+									left: level.holes.top.right,
+									right: level.holes.top.left,
+								},
+								bottom: {
+									left: level.holes.bottom.right,
+									right: level.holes.bottom.left,
+								},
+							},
+						})
+					}
+				>
+					{icons.horizontalArrows}
+				</button>
+			</ButtonGroup>
+			<ButtonGroup>
+				<button onClick={() => props.setLevel(scrollLevelX("left", level))}>
+					{icons.arrowLeft}
+				</button>
+				<button onClick={() => props.setLevel(scrollLevelY("up", level))}>
+					{icons.arrowUp}
+				</button>
+				<button onClick={() => props.setLevel(scrollLevelY("down", level))}>
+					{icons.arrowDown}
+				</button>
+				<button onClick={() => props.setLevel(scrollLevelX("right", level))}>
+					{icons.arrowRight}
+				</button>
+			</ButtonGroup>
+		</ButtonRow>
+	);
 };
 
 export function getTileCoord(eventCoord: Coord2): Coord2 {
@@ -67,4 +156,91 @@ export function createSetSomeTiles(
 
 		setTiles(newTiles);
 	};
+}
+
+function scrollLevelY(direction: "up" | "down", level: Level) {
+	const yDirection = {
+		up: -1,
+		down: 1,
+	}[direction];
+
+	const newLevel = {
+		...level,
+		platformTiles: assertTuple(
+			{
+				up: () =>
+					[
+						level.platformTiles.slice(1),
+						level.platformTiles.slice(0, 1),
+					].flat(),
+				down: () =>
+					[
+						level.platformTiles.slice(-1),
+						level.platformTiles.slice(0, -1),
+					].flat(),
+			}[direction](),
+			platformTilesSize.y
+		),
+		monsters: level.monsters.map((monster) => ({
+			...monster,
+			spawnPoint: {
+				...monster.spawnPoint,
+				y:
+					((monster.spawnPoint.y -
+						4 * 8 +
+						(platformTilesSize.y + yDirection) * 8) %
+						(platformTilesSize.y * 8)) +
+					4 * 8,
+			},
+		})),
+		itemSpawnPositions: mapRecord(level.itemSpawnPositions, (q) => ({
+			...q,
+			y: (q.y + platformTilesSize.y + yDirection) % platformTilesSize.y,
+		})),
+	};
+	return newLevel;
+}
+
+function scrollLevelX(direction: "left" | "right", level: Level) {
+	const xDirection = {
+		left: -1,
+		right: 1,
+	}[direction];
+
+	const newLevel = {
+		...level,
+		platformTiles: {
+			left: () =>
+				mapTuple(level.platformTiles, (row) =>
+					assertTuple(
+						[row.slice(1), row.slice(0, 1)].flat(),
+						platformTilesSize.x
+					)
+				),
+			right: () =>
+				mapTuple(level.platformTiles, (row) =>
+					assertTuple(
+						[row.slice(-1), row.slice(0, -1)].flat(),
+						platformTilesSize.x
+					)
+				),
+		}[direction](),
+		monsters: level.monsters.map((monster) => ({
+			...monster,
+			spawnPoint: {
+				...monster.spawnPoint,
+				x:
+					((monster.spawnPoint.x -
+						4 * 8 +
+						(platformTilesSize.x + xDirection) * 8) %
+						(platformTilesSize.x * 8)) +
+					4 * 8,
+			},
+		})),
+		itemSpawnPositions: mapRecord(level.itemSpawnPositions, (q) => ({
+			...q,
+			x: (q.x + platformTilesSize.x + xDirection) % platformTilesSize.x,
+		})),
+	};
+	return newLevel;
 }
--- src/ui/icons.tsx
+++ src/ui/icons.tsx
@@ -183,6 +183,9 @@ export const icons = {
 		</svg>
 	),
 	arrowUp: <Arrow rotation={0} />,
+	arrowRight: <Arrow rotation={90} />,
+	arrowDown: <Arrow rotation={180} />,
+	arrowLeft: <Arrow rotation={270} />,
 	// https://icons.getbootstrap.com/icons/copy/
 	copy: (
 		<svg
@@ -338,6 +341,36 @@ export const icons = {
 			viewBox="0 0 16 16"
 		>
 			<path d="M9.302 1.256a1.5 1.5 0 0 0-2.604 0l-1.704 2.98a.5.5 0 0 0 .869.497l1.703-2.981a.5.5 0 0 1 .868 0l2.54 4.444-1.256-.337a.5.5 0 1 0-.26.966l2.415.647a.5.5 0 0 0 .613-.353l.647-2.415a.5.5 0 1 0-.966-.259l-.333 1.242zM2.973 7.773l-1.255.337a.5.5 0 1 1-.26-.966l2.416-.647a.5.5 0 0 1 .612.353l.647 2.415a.5.5 0 0 1-.966.259l-.333-1.242-2.545 4.454a.5.5 0 0 0 .434.748H5a.5.5 0 0 1 0 1H1.723A1.5 1.5 0 0 1 .421 12.24zm10.89 1.463a.5.5 0 1 0-.868.496l1.716 3.004a.5.5 0 0 1-.434.748h-5.57l.647-.646a.5.5 0 1 0-.708-.707l-1.5 1.5a.5.5 0 0 0 0 .707l1.5 1.5a.5.5 0 1 0 .708-.707l-.647-.647h5.57a1.5 1.5 0 0 0 1.302-2.244z" />
+		</svg>
+	),
+	// https://icons.getbootstrap.com/icons/arrow-down-up/
+	verticalArrows: (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width="16"
+			height="16"
+			fill="currentColor"
+			viewBox="0 0 16 16"
+		>
+			<path
+				fillRule="evenodd"
+				d="M11.5 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L11 2.707V14.5a.5.5 0 0 0 .5.5m-7-14a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L4 13.293V1.5a.5.5 0 0 1 .5-.5"
+			/>
+		</svg>
+	),
+	// https://icons.getbootstrap.com/icons/arrow-left-right/
+	horizontalArrows: (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width="16"
+			height="16"
+			fill="currentColor"
+			viewBox="0 0 16 16"
+		>
+			<path
+				fillRule="evenodd"
+				d="M1 11.5a.5.5 0 0 0 .5.5h11.793l-3.147 3.146a.5.5 0 0 0 .708.708l4-4a.5.5 0 0 0 0-.708l-4-4a.5.5 0 0 0-.708.708L13.293 11H1.5a.5.5 0 0 0-.5.5m14-7a.5.5 0 0 1-.5.5H2.707l3.147 3.146a.5.5 0 1 1-.708.708l-4-4a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 4H14.5a.5.5 0 0 1 .5.5"
+			/>
 		</svg>
 	),
 	// https://icons.getbootstrap.com/icons/dice-5/
