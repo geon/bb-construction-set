7552c543c569947387c04e6e558570c27f0d85bd
Victor Widell <victor.widell@gmail.com>
2025-12-24 18:58:01 +0100

    Added commented out palette.

--- src/bb/internal-data-formats/palette.ts
+++ src/bb/internal-data-formats/palette.ts
@@ -22,12 +22,16 @@ export type PaletteIndex =
 export type Palette = Tuple<Color, 16>;
 
 export const rgbPalette: Palette = mapTuple(
-	// https://www.c64-wiki.com/wiki/Color
 	[
 		// https://www.pepto.de/projects/colorvic/2001
 		0x000000, 0xffffff, 0x68372b, 0x70a4b2, 0x6f3d86, 0x588d43, 0x352879,
 		0xb8c76f, 0x6f4f25, 0x433900, 0x9a6759, 0x444444, 0x6c6c6c, 0x9ad284,
 		0x6c5eb5, 0x959595,
+
+		// https://www.c64-wiki.com/wiki/Color
+		// 0x000000, 0xffffff, 0x880000, 0xaaffee, 0xcc44cc, 0x00cc55, 0x0000aa,
+		// 0xeeee77, 0xdd8855, 0x664400, 0xff7777, 0x333333, 0x777777, 0xaaff66,
+		// 0x0088ff, 0xbbbbbb,
 	],
 	hexToRgb
 );
